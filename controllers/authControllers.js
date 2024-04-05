b7a5a1f6bd7d83a791a89ff4a7676c8640951d6b
NataliaPot <137684728+NataliaPot@users.noreply.github.com>
2024-04-05 15:23:14 +0200

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -88,7 +88,13 @@ export const updateSubscription = async (req, res) => {
 
 export const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+
+  if (!req.file) {
+    return res.status(400).json({ message: "No file for update" });
+  }
+
   const { path: tempUpload, originalname } = req.file;
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
 
